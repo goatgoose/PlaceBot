e5c397cd3c2d490514864db68f047aa151329f89
unknown <3758302+goatgoose@users.noreply.github.com>
2022-04-02 03:49:22 -0400

    fix place tile example

--- examples/place_tile.py
+++ examples/place_tile.py
@@ -2,7 +2,7 @@ import sys
 from place_bot import Placer, Color
 
 if len(sys.argv) != 3:
-    print("usage: python3 place_bot.py [reddit username] [reddit password]")
+    print("usage: python3 place_tile.py [reddit username] [reddit password]")
     exit()
 
 username = sys.argv[1]
