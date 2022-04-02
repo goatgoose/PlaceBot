862c43ab66e2ea226b9fb7018be3ddb7512364c3
unknown <3758302+goatgoose@users.noreply.github.com>
2022-04-02 18:42:21 -0400

    cleanup

---
+++ place_bot/color.py
@@ -0,0 +1,56 @@
+class Color:
+    def __init__(self, name: str, value: int, hex_: str):
+        self.name = name
+        self.value = value
+        self.hex = hex_
+
+    @staticmethod
+    def rgb2hex(r, g, b):
+        return f"#{r:02x}{g:02x}{b:02x}"
+
+    @staticmethod
+    def colors_by_id():
+        return {
+            1: Color("DARK_RED", 1, "BE0039"),
+            2: Color("RED", 2, "FF4500"),
+            3: Color("ORANGE", 3, "FFA800"),
+            4: Color("YELLOW", 4, "FFD635"),
+            6: Color("DARK_GREEN", 6, "00A368"),
+            7: Color("GREEN", 7, "00CC78"),
+            8: Color("LIGHT_GREEN", 8, "7EED56"),
+            9: Color("DARK_TEAL", 9, "00756F"),
+            10: Color("TEAL", 10, "009EAA"),
+            12: Color("DARK_BLUE", 12, "2450A4"),
+            13: Color("BLUE", 13, "3690EA"),
+            14: Color("LIGHT_BLUE", 14, "51E9F4"),
+            15: Color("INDIGO", 15, "493AC1"),
+            16: Color("PERIWINKLE", 16, "6A5CFF"),
+            18: Color("DARK_PURPLE", 18, "811E9F"),
+            19: Color("PURPLE", 19, "B44AC0"),
+            20: Color("PINK", 20, "FF3881"),
+            23: Color("LIGHT_PINK", 23, "FF99AA"),
+            25: Color("BROWN", 25, "6D482F"),
+            26: Color("DARK_BROWN", 26, "9C6926"),
+            27: Color("BLACK", 27, "000000"),
+            29: Color("GRAY", 29, "898D90"),
+            30: Color("LIGHT_GRAY", 30, "D4D7D9"),
+            31: Color("WHITE", 31, "FFFFFF"),
+        }
+
+    @staticmethod
+    def colors_by_name():
+        return {c.name: c for c in Color.colors_by_id().values()}
+
+    @staticmethod
+    def colors_by_hex():
+        return {c.hex: c for c in Color.colors_by_id().values()}
+
+    @staticmethod
+    def from_pixel(rgb_im_px) -> 'Color':
+        r, g, b = rgb_im_px
+        hex_ = Color.rgb2hex(r, g, b)
+        return Color.colors_by_hex()[hex_.upper().strip("#")]
+
+    @staticmethod
+    def from_name(name: str):
+        return Color.colors_by_name().get(name.upper())
--- place_bot/place_bot.py
+++ place_bot/place_bot.py
@@ -11,58 +11,7 @@ import numpy as np
 from typing import Tuple
 
 from . import queries
-
-from dataclasses import dataclass
-
-
-def rgb2hex(r, g, b):
-    return f"#{r:02x}{g:02x}{b:02x}"
-
-
-@dataclass
-class Color:
-    name: str
-    value: int
-    hex: str
-
-
-# fmt: off
-colors_by_id = {
-    1: Color("DARK_RED",     1,  "BE0039"),
-    2: Color("RED",          2,  "FF4500"),
-    3: Color("ORANGE",       3,  "FFA800"),
-    4: Color("YELLOW",       4,  "FFD635"),
-    6: Color("DARK_GREEN",   6,  "00A368"),
-    7: Color("GREEN",        7,  "00CC78"),
-    8: Color("LIGHT_GREEN",  8,  "7EED56"),
-    9: Color("DARK_TEAL",    9,  "00756F"),
-    10: Color("TEAL",        10, "009EAA"),
-    12: Color("DARK_BLUE",   12, "2450A4"),
-    13: Color("BLUE",        13, "3690EA"),
-    14: Color("LIGHT_BLUE",  14, "51E9F4"),
-    15: Color("INDIGO",      15, "493AC1"),
-    16: Color("PERIWINKLE",  16, "6A5CFF"),
-    18: Color("DARK_PURPLE", 18, "811E9F"),
-    19: Color("PURPLE",      19, "B44AC0"),
-    20: Color("PINK",        20, "FF3881"),
-    23: Color("LIGHT_PINK",  23, "FF99AA"),
-    25: Color("BROWN",       25, "6D482F"),
-    26: Color("DARK_BROWN",  26, "9C6926"),
-    27: Color("BLACK",       27, "000000"),
-    29: Color("GRAY",        29, "898D90"),
-    30: Color("LIGHT_GRAY",  30, "D4D7D9"),
-    31: Color("WHITE",       31, "FFFFFF"),
-}
-# fmt: on
-
-colors_by_name = {c.name: c for c in colors_by_id.values()}
-colors_by_hex = {c.hex: c for c in colors_by_id.values()}
-
-
-def color_from_pixel(rgb_im_px) -> Color:
-    r, g, b = rgb_im_px
-    hex_ = rgb2hex(r, g, b)
-    return colors_by_hex[hex_.upper().strip("#")]
+from .color import Color
 
 
 class Placer:
@@ -74,9 +23,7 @@ class Placer:
         "accept-language": "en-US,en;q=0.9",
         "content-type": "application/x-www-form-urlencoded",
         "origin": REDDIT_URL,
-        "sec-ch-ua": (
-            '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"'
-        ),
+        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-platform": '"Windows"',
         "sec-fetch-dest": "empty",
@@ -119,8 +66,8 @@ class Placer:
         r = self.client.get(self.REDDIT_URL)
         data_str = (
             BeautifulSoup(r.content, "html.parser")
-            .find("script", {"id": "data"})
-            .contents[0][len("window.__r = ") : -1]
+                .find("script", {"id": "data"})
+                .contents[0][len("window.__r = "): -1]
         )
         data = json.loads(data_str)
         self.token = data["user"]["session"]["accessToken"]
@@ -175,9 +122,7 @@ class Placer:
 
         return map_data
 
-    def maintain_image(
-        self, x: int, y: int, image_path: str, image_shape: Tuple[int, int]
-    ):
+    def maintain_image(self, x: int, y: int, image_path: str, image_shape: Tuple[int, int]):
         """
         :param x: left most x coord
         :param y: top most y coord
@@ -189,16 +134,14 @@ class Placer:
 
         while True:
             map_data = self.get_map_data()
-            map_slice = map_data[y : y + image_shape[0], x : x + image_shape[1]]
+            map_slice = map_data[y: y + image_shape[0], x: x + image_shape[1]]
 
-            differing_pixels = self._find_differing_pixels(
-                map_slice, im_data, image_shape
-            )
+            differing_pixels = self._find_differing_pixels(map_slice, im_data, image_shape)
             if len(differing_pixels) > 0:
                 differing_pixel = differing_pixels[0]
                 x_ = differing_pixel[0]
                 y_ = differing_pixel[1]
-                self.place_tile(x + x_, y + y_, colors_by_id[im_data[y_, x_]])
+                self.place_tile(x + x_, y + y_, Color.colors_by_id()[im_data[y_, x_]])
                 time.sleep(60 * 5 + 10)
             else:
                 time.sleep(30)
@@ -218,43 +161,37 @@ class Placer:
     def image_to_data(image: Image, shape: Tuple[int, int]):
         rgb_im = image.convert("RGB")
 
-        data = np.array([color_from_pixel(px).value for px in rgb_im.getdata()])
+        data = np.array([Color.from_pixel(px).value for px in rgb_im.getdata()])
         data = np.reshape(data, shape)
 
         return data
 
     def _get_map_url(self):
         ws = websocket.create_connection("wss://gql-realtime-2.reddit.com/query")
-        ws.send(
-            json.dumps(
-                {
-                    "type": "connection_init",
-                    "payload": {"Authorization": "Bearer " + self.token},
-                }
-            )
-        )
-        ws.send(
-            json.dumps(
-                {
-                    "type": "start",
-                    "id": "1",
-                    "payload": {
-                        "extensions": {},
-                        "operationName": "replace",
-                        "query": queries.FULL_FRAME_MESSAGE_SUBSCRIBE_QUERY,
-                        "variables": {
-                            "input": {
-                                "channel": {
-                                    "category": "CANVAS",
-                                    "tag": "0",
-                                    "teamOwner": "AFD2022",
-                                }
-                            }
-                        },
-                    },
-                }
-            )
-        )
+        ws.send(json.dumps({
+            "type": "connection_init",
+            "payload": {
+                "Authorization": "Bearer " + self.token
+            },
+        }))
+        ws.send(json.dumps({
+            "type": "start",
+            "id": "1",
+            "payload": {
+                "extensions": {},
+                "operationName": "replace",
+                "query": queries.FULL_FRAME_MESSAGE_SUBSCRIBE_QUERY,
+                "variables": {
+                    "input": {
+                        "channel": {
+                            "category": "CANVAS",
+                            "tag": "0",
+                            "teamOwner": "AFD2022",
+                        }
+                    }
+                },
+            },
+        }))
 
         while True:
             result = json.loads(ws.recv())
@@ -263,8 +200,9 @@ class Placer:
             if result["id"] != "1":
                 continue
             assert (
-                result["payload"]["data"]["subscribe"]["data"]["__typename"]
-                == "FullFrameMessageData"
+                    result["payload"]["data"]["subscribe"]["data"]["__typename"]
+                    == "FullFrameMessageData"
             )
+
             ws.close()
             return result["payload"]["data"]["subscribe"]["data"]["name"]
